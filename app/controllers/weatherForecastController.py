a25449d21c356b9c23c55710d82ca619e57e6839
Marco Ferreira <2119022@student.uma.pt>
2025-10-02 11:30:01 +0100

    refactor: Return JSONResponse in weather forecast controller

--- app/controllers/weatherForecastController.py
+++ app/controllers/weatherForecastController.py
@@ -1,16 +1,20 @@
-
+from fastapi.responses import JSONResponse
 from app.modules.IPMAWebScraper import get_weather_forecast, parse_response
 def getWeatherForeCast(req: dict ):
     awnser = get_weather_forecast(req['destrict'], req['city'])
     weather_data = parse_response(awnser, req['destrict'], req['city'])
 
-
-    apireturn_data = {}
     if req['day'] == "all":
-        apireturn_data = weather_data
+        return JSONResponse(status_code=200, content={"status": "ok", "data": weather_data})
+
     else:
         if req['day'] not in weather_data:
-            return {"status": "error", "message": f"Day is not available in IPMA. Only Days - (" + ", ".join(weather_data.keys()) + " )"}
-        apireturn_data = weather_data[req['day']]
+            return JSONResponse(
+                status_code=404,
+                content={
+                    "status": "error",
+                    "message": f"Day is not available in IPMA web, Only Days - (" + ", ".join(weather_data.keys()) + " )"
+                }
+            )
 
-    return {"status": "ok", "data": apireturn_data}
+        return JSONResponse(status_code=200, content={"status": "ok", "data": weather_data[req['day']]})
